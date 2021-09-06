18ea129e2b9ba09089de456069135f82b62c5f56
Ocean12AUG <90170331+Ocean12AUG@users.noreply.github.com>
2021-09-06 11:43:09 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
     introTitle: 'Cục cưng à!',
     introDesc: `Nếu em không ngại 
-    Thì để ngày mai anh qua thưa ba má em cho mình làm quenn
-    Biết đâu sớm mai thức dậy nhìn thấy nhau
+    thì để ngày mai anh qua thưa ba má em cho mình làm quenn,
+    biết đâu sớm mai thức dậy nhìn thấy nhau
     Cất lên tiếng gọi “Mình ơi!”`,
     btnIntro: 'Nghĩ thôi cũng vui,hihi',
-    title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là yêu emm, cưới emm <33',
-    btnNo: 'Không, bé yêu à :3',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là yêu anh, cưới anh nhó <33',
+    btnNo: 'Không, yêu à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    messDesc: 'Ráng đợi hết dịch gòi tụi mình vào làm đứa nhó',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
